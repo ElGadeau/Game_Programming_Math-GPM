2a6958d4268b75872481154579904a8f62381202
Thibaut PONCHON <t.ponchon@student.isartdigital.com>
2019-11-20 12:07:17 -0500

    Vector4 constructors added and main are using try catch

--- GPM/GPM/include/GPM/Vector/Vector4.h
+++ GPM/GPM/include/GPM/Vector/Vector4.h
@@ -1,7 +1,70 @@
 #pragma once
- // Make your .inl here in include folder.
+// Make your .inl here in include folder.
 
-struct Vector4
+#include <type_traits>
+
+namespace GPM
 {
-	
-};
+	template<typename T>
+	struct Vector4
+	{
+		static_assert(std::is_arithmetic<T>::value, "Vector4 should only be used with arithmetic types");
+		
+		T x;
+		T y;
+		T z;
+		T w;
+
+		/**
+ * Default Constructor
+ */
+		constexpr Vector4();
+
+		/**
+		 * Constructor with parameters
+		 * @param p_x
+		 * @param p_y
+		 * @param p_z
+		 * @param p_w Set to 1 for vectors, 0 for points
+		 */
+		constexpr Vector4(T p_x, T p_y, T p_z, T p_w = 1.0f);
+
+		/**
+		* Constructor from vector3
+		* @param p_other
+		*/
+		//explicit constexpr Vector4(const Vector3<T>& p_other);
+
+		/**
+		* Move Constructor from vector3
+		* @param p_other
+		*/
+		//explicit constexpr Vector4(Vector3<T>&& p_other);
+
+		/**
+		 * Copy Constructor
+		 * @param p_other
+		 */
+		constexpr Vector4(const Vector4<T>& p_other);
+
+		/**
+		 * Move Constructor
+		 * @param p_other
+		 */
+		constexpr Vector4(Vector4<T>&& p_other) noexcept;
+
+		/**
+		 * Overload = operator by copy
+		 * @param p_other
+		 */
+		constexpr Vector4<T>& operator=(const Vector4<T>& p_other);
+
+		/**
+		 * Overload = operator by move
+		 * @param p_other
+		 */
+		constexpr Vector4<T>& operator=(Vector4<T>&& p_other) noexcept;
+	};
+}
+
+#include "Vector4.inl"
--- GPM/MathTestProject/src/main.cpp
+++ GPM/MathTestProject/src/main.cpp
@@ -3,6 +3,14 @@
 
 int main(const int p_argc, char** p_argv)
 {
-
+	try
+	{
+		// Your tests here
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception thrown: " << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
